77be3c8e70301050819a6ff1f9f4dbeb346f6184
mbadox45 <mbadox45@gmail.com>
2026-05-18 15:17:48 +0700

    feat: update konfigurasi PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,11 @@ module.exports = {
   apps: [
     {
       name: "tax-activity-backend",
-      // Gunakan modul uvicorn langsung melalui interpreter venv
-      script: "source venv/bin/activate",
-      args: "uvicorn app.main:app --reload --port 3032 --workers 4",
-      // Arahkan interpreter ke python bawaan venv Anda secara absolut
-      interpreter: "/home/ict-production/Documents/tax/Tax-Activity-Backend/venv/bin/python3",
+      // Tunjuk langsung ke binary uvicorn di dalam venv Anda
+      script: "/home/ict-production/Documents/tax/Tax-Activity-Backend/venv/bin/uvicorn",
+      // Masukkan argumen aplikasi di sini
+      args: "app.main:app --host 0.0.0.0 --port 8000 --workers 4",
+      // Paksa PM2 untuk mengeksekusinya sebagai script biasa (bukan fork python global)
       exec_mode: "fork",
       instances: 1,
       autorestart: true,
